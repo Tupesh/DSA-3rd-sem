ad94183daac04cc68802d12b942f2227675ad680
Tupesh <tupeshghimire@gmai.com>
2024-03-07 14:17:48 +0545

    node adding to last

---
+++ adding2LastLinkedList.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+int main(){
+    struct node * one=(struct node *)malloc(sizeof(struct node));
+    struct node * two=(struct node *)malloc(sizeof(struct node));
+    struct node * three=(struct node *)malloc(sizeof(struct node));
+    
+    
+    struct node * head=one;
+
+    // Assigning data's value
+    one->data=34;
+    two->data=68;
+    three->data=52;
+    
+    // Assigning pointer reference
+    one->next=two;
+    two->next=three;
+    three->next=NULL;
+
+    // Temporary node for loop
+    struct node * temp=head;
+    
+    //Looping through the node
+    while (temp!=NULL)
+    {
+        printf("%d \t %d \n",temp->data,temp->next);
+        temp=temp->next;
+    }
+    
+
+
+}
