1da52b3e0dbbebc746b78dccb24070a76152551c
Tupesh <tupeshghimire@gmai.com>
2024-03-07 15:34:46 +0545

    double linked list

---
+++ B_doubleLinkedlList/doublelinedlist.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+int main()
+{
+    struct node *one = (struct node *)malloc(sizeof(struct node));
+    struct node *two = (struct node *)malloc(sizeof(struct node));
+    struct node *three = (struct node *)malloc(sizeof(struct node));
+
+    // Defining head and tail of a node
+    struct node *head = one;
+    struct node *tail = three;
+
+    // Assigning data's value
+    one->data = 364;
+    two->data = 968;
+    three->data = 752;
+
+    // Assigning pointer reference to next
+    one->next = two;
+    two->next = three;
+    three->next = NULL;
+
+    // Assigning pointer reference to previous
+    three->prev = two;
+    two->prev = one;
+    one->prev = NULL;
+
+    // Temporary node for loop through head node
+    struct node *tempNext = head;
+
+    // Temporary node for loop through tail node
+    struct node *tempPrev = tail;
+
+    // Looping through the node from 1st to 2nd and so on
+    printf("Looping from head\n");
+    while (tempNext != NULL)
+    {
+        printf("%d \t %d \n", tempNext->data, tempNext->next);
+        tempNext = tempNext->next;
+    }
+
+    // Looping through the node from 3rd to 2nd and so on
+    printf("\nLooping from tail\n");
+    while (tempPrev != NULL)
+    {
+        printf("%d \t %d \n", tempPrev->data, tempPrev->prev);
+        tempPrev = tempPrev->prev;
+    }
+}
