64999091220b6902576f8447d04d2248ab2d28c1
Tupesh <tupesghimire@gmail.com>
2024-03-25 16:53:20 +0545

    deleting node at position

--- A_LINKED_LIST/D_linkedListWithFunction/2_addingInBetween.cpp
+++ A_LINKED_LIST/D_linkedListWithFunction/2_addingInBetween.cpp
@@ -1,4 +1,3 @@
-// adding new between nodes
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -28,49 +27,72 @@ void insert(int datum)
         tail = newNode;
     }
 }
-void insertatposition(int position, int data){
+
+void insertatposition(int position, int data)
+{
+    if (position < 1)
+    {
+        printf("Invalid position\n");
+        return;
+    }
+
     struct node *temp = head;
     struct node *newNode = (struct node *)malloc(sizeof(struct node));
     newNode->data = data;
+    newNode->next = NULL;
 
-for(int i = 1; i <position; i++){
-    temp = temp-> next;
-}
-
-newNode->next=temp->next;
-temp->next=newNode;
+    // If position is 1, insert at the beginning
+    if (position == 1)
+    {
+        newNode->next = head;
+        head = newNode;
+        return;
     }
+
+    // Traverse to the node before the specified position
+    for (int i = 1; i < position - 1 && temp != NULL; i++)
+    {
+        temp = temp->next;
+    }
+
+    // Check if position is valid
+    if (temp == NULL)
+    {
+        printf("Invalid position\n");
+        free(newNode); // Free the allocated memory for the new node
+        return;
+    }
+
+    // Insert newNode between temp and temp->next
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
 
 int main()
 {
     insert(5);
     insert(10);
     insert(15);
-    
+
     struct node *temp = head;
-    while (true)
+    printf("Original Linked List:\n");
+    while (temp != NULL)
     {
-        printf("%d | %p | \n", temp->data, temp->next);
+        printf("%d | %p |\n", temp->data, temp->next);
         temp = temp->next;
-        if (temp==NULL){
-            break;
-        }
     }
-    insertatposition(2,25);
-    insertatposition(3,35);
-    insertatposition(1,45);
+
+    insertatposition(2, 25);
+    insertatposition(3, 35);
+    insertatposition(1, 45);
 
     temp = head;
-    printf("Linked List:\n");
-    while (true)
+    printf("\nLinked List after insertions:\n");
+    while (temp != NULL)
     {
-        printf("%d | %p | \n", temp->data, temp->next);
+        printf("%d | %p |\n", temp->data, temp->next);
         temp = temp->next;
-        if (temp==NULL){
-            break;
-        }
     }
-
 
     return 0;
 }
