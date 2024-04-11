ee004e3bd5e60f73a170c10cd5df9d262d6024c8
Tupesh <tupesghimire@gmail.com>
2024-04-11 15:01:40 +0545

    circular queue

---
+++ C_QUEUE/C_circularQueue.cpp
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define maxsize 5
+
+int front = -1;
+int rear = -1;
+int queue[maxsize];
+
+bool isEmpty()
+{
+    if (front == -1 && rear == -1)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isFull()
+{
+    if ((front == rear + 1) || (front == 0 && rear == maxsize - 1))
+    {
+        return true;
+    }
+    return false;
+}
+
+void enqueue(int x)
+{
+    if (isFull())
+    {
+        printf("Queue is full\n\n");
+    }
+    else if (isEmpty())
+    {
+        rear = front = 0;
+        queue[rear] = x;
+    }
+    else
+    {
+        rear = (rear + 1) % maxsize;
+        queue[rear] = x;
+    }
+}
+
+void dequeue()
+{
+    if (isEmpty())
+    {
+
+        printf("Queue is empty\n");
+    }
+    else if (front == rear)
+    {
+        front = rear = -1;
+    }
+    else
+    {
+        front = (front + 1) % maxsize;
+    }
+}
+
+void print()
+{
+    int i;
+    if (isEmpty())
+    {
+        printf("Queue is empty\n");
+    }
+    else
+    {
+        for (i = front; i != rear; i = (i + 1) % maxsize)
+        {
+            printf("%d\n", queue[i]);
+        }
+        printf("%d\n", queue[i]); /* To print the rear element of queue */
+        printf("front: %d\trear: %d\n", front, rear);
+        printf("\n\n");
+    }
+}
+
+void frontValue()
+{
+    printf("%d", front);
+}
+
+void rearValue()
+{
+    printf("%d", rear);
+}
+
+int main()
+{
+    enqueue(1);
+    enqueue(2);
+    enqueue(3);
+    enqueue(4);
+    enqueue(5);
+    print();
+    enqueue(6);
+    dequeue();
+    dequeue();
+    dequeue();
+    print();
+    enqueue(6);
+    enqueue(7);
+    enqueue(8);
+    print();
+    enqueue(9);
+}
